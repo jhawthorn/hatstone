b1dd44a1bc10c9d76c154d778f85c06fe78a422f
John Hawthorn <john@hawthorn.email>
2023-04-24 23:01:00 -0700

    Also test x64

--- test/hatstone_test.rb
+++ test/hatstone_test.rb
@@ -1,7 +1,22 @@
 require "helper"
 
 class HatstoneTest < Hatstone::Test
-  def test_disasm
+  def test_disasm_x64
+    hs = Hatstone.new(Hatstone::ARCH_X86, Hatstone::MODE_64)
+
+    # https://godbolt.org/z/W95G3zTh1
+    insns = +""
+    insns << "\xb8\x2a\x00\x00\x00" # mov eax, 42
+    insns << "\xc3" # mov eax, 42
+
+    disassembled = hs.disasm(insns, 0x0)
+    assert_equal "mov", disassembled[0].mnemonic
+    assert_equal "eax, 0x2a", disassembled[0].op_str
+    assert_equal "ret", disassembled[1].mnemonic
+    assert_equal "", disassembled[1].op_str
+  end
+
+  def test_disasm_arm
     hs = Hatstone.new(Hatstone::ARCH_ARM64, Hatstone::MODE_ARM)
 
     # Assemble some instructions
@@ -12,7 +27,9 @@ class HatstoneTest < Hatstone::Test
 
     disassembled = hs.disasm(insns, 0x0)
     assert_equal "movz", disassembled[0].mnemonic
+    assert_equal "x0, #0x2a", disassembled[0].op_str
     assert_equal "ret", disassembled[1].mnemonic
+    assert_equal "", disassembled[1].op_str
   end
 
   # ARM instructions
